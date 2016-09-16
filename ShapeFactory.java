4ffe9d893a7fa7904837743fa1fb6850b95f744a
ProjectDybrova <alex_3257@mail.ru>
2016-09-16 10:48:10 +0300

    4. fix bugs and improve the code

--- ShapeFactory.java
+++ ShapeFactory.java
@@ -14,83 +14,90 @@ import java.awt.geom.Rectangle2D;
 
 public class ShapeFactory {
     public Shape shape;
-    public BasicStroke stroke = new BasicStroke(7.0f);
+    public BasicStroke stroke = new BasicStroke(3.0f);
     public Paint paint;
     public int width = 25;
     public int height = 25;
 
-    public ShapeFactory(int shape_type) {
-/** выбираем форму фигуры и толщину линии
-         * @param shape_type - число для выполнения задания согласно варианту         
-        */
-
-        switch (shape_type / 10) {
-
-            case 1: {
-                this.shape = ShapeFactory.createStar(3, new Point(0, 0), (double)this.width / 2.0, (double)this.width / 2.0);
-                break;
-            }
-            case 3: {
-                this.shape = ShapeFactory.createStar(5, new Point(0, 0), (double)this.width / 2.0, (double)this.width / 4.0);
-                break;
-            }
-            case 5: {
-                this.shape = new Rectangle2D.Double((double)(- this.width) / 2.0, (double)(- this.height) / 2.0, this.width, this.height);
-                break;
-            }
-            case 7: {
-                GeneralPath path = new GeneralPath();
-                double tmp_height = Math.sqrt(2.0) / 2.0 * (double)this.height;
-                path.moveTo((double)((- this.width) / 2), - tmp_height);
-                path.lineTo(0.0, - tmp_height);
-                path.lineTo((double)(this.width / 2), tmp_height);
-                path.closePath();
-                this.shape = path;
-                break;
-            }
-            case 9: {
-                this.shape = new Arc2D.Double((double)(- this.width) / 2.0, (double)(- this.height) / 2.0, this.width, this.height, 30.0, 300.0, 2);
-                break;
-            }
-            default: {
-                throw new Error("type is nusupported");
-            }
-        }
-        switch (shape_type % 10) {
-
-            case 1: {
-                this.stroke = new BasicStroke(3.0f);
-                break;
-            }
-            case 3: {
-                break;
-            }
-            case 4: {
-                this.stroke = new BasicStroke(7.0f);
-                break;
-            }
-            case 7: {
-                this.paint = new GradientPaint(- this.width, - this.height, Color.white, this.width, this.height, Color.gray, true);
-                break;
-            }
-            case 8: {
-                this.paint = Color.red;
-                break;
-            }
-            default: {
-                throw new Error("type is nusupported");
-            }
-        }
+    public ShapeFactory(String TypeOfChoiseFigure, int BorderThickness) 
+    {
+    	this.stroke = new BasicStroke(BorderThickness);
+    	if (TypeOfChoiseFigure == "Hexagon")
+    	{
+    		this.shape = ShapeFactory.createStar(3, new Point(0, 0), (double)this.width / 2.0, (double)this.width / 2.0);
+    	}
+    	else if (TypeOfChoiseFigure == "Five-pointed_star") 
+    	{
+    		this.shape = ShapeFactory.createStar(5, new Point(0, 0), (double)this.width / 2.0, (double)this.width / 4.0);
+    	}
+    	else if (TypeOfChoiseFigure == "Rectangle") 
+    	{
+    		this.shape = new Rectangle2D.Double((double)(- this.width) / 2.0, (double)(- this.height) / 2.0, this.width, this.height);
+    	}
+    	else if (TypeOfChoiseFigure == "Triangle") 
+    	{
+    		GeneralPath path = new GeneralPath();
+            double tmp_height = Math.sqrt(2.0) / 2.0 * (double)this.height;
+            path.moveTo((double)((- this.width) / 2), - tmp_height);
+            path.lineTo(0.0, - tmp_height);
+            path.lineTo((double)(this.width / 2), tmp_height);
+            path.closePath();
+            this.shape = path;
+    	}
+    	else if (TypeOfChoiseFigure == "Arc") 
+    	{
+    		this.shape = new Arc2D.Double((double)(- this.width) / 2.0, (double)(- this.height) / 2.0, this.width, this.height, 30.0, 300.0, 2);
+    	}
+    	else 
+    	{
+    		throw new Error("type is nusupported");
+    	}        
+    }
+    public ShapeFactory(String TypeOfChoiseFigure, int BorderThickness, boolean Gradient, boolean ColorRed) 
+    {
+    	this.stroke = new BasicStroke(BorderThickness);    	
+    	if (TypeOfChoiseFigure == "Hexagon")
+    	{
+    		this.shape = ShapeFactory.createStar(3, new Point(0, 0), (double)this.width / 2.0, (double)this.width / 2.0);
+    	}
+    	else if (TypeOfChoiseFigure == "Five-pointed_star") 
+    	{
+    		this.shape = ShapeFactory.createStar(5, new Point(0, 0), (double)this.width / 2.0, (double)this.width / 4.0);
+    	}
+    	else if (TypeOfChoiseFigure == "Rectangle") 
+    	{
+    		this.shape = new Rectangle2D.Double((double)(- this.width) / 2.0, (double)(- this.height) / 2.0, this.width, this.height);
+    	}
+    	else if (TypeOfChoiseFigure == "Triangle") 
+    	{
+    		GeneralPath path = new GeneralPath();
+            double tmp_height = Math.sqrt(2.0) / 2.0 * (double)this.height;
+            path.moveTo((double)((- this.width) / 2), - tmp_height);
+            path.lineTo(0.0, - tmp_height);
+            path.lineTo((double)(this.width / 2), tmp_height);
+            path.closePath();
+            this.shape = path;
+    	}
+    	else if (TypeOfChoiseFigure == "Arc") 
+    	{
+    		this.shape = new Arc2D.Double((double)(- this.width) / 2.0, (double)(- this.height) / 2.0, this.width, this.height, 30.0, 300.0, 2);
+    	}
+    	else 
+    	{
+    		throw new Error("type is nusupported");
+    	}
+       
+    	if (Gradient == true) 
+    	{
+    		this.paint = new GradientPaint(- this.width, - this.height, Color.white, this.width, this.height, Color.gray, true);
+    	}  	
+    	if (ColorRed == true) 
+    	{
+    		this.paint = Color.red;
+    	}   	   
     }
 
     private static Shape createStar(int arms, Point center, double rOuter, double rInner) {
-/** Расчитывает координаты и рисует звезу
-         * @param arms - количество вершин
-         * @param center - координаты центра
-         * @param rOuter - радиус на котором располагаются внешние вершины
-         * @param rInner - радиус на котором располагаются внутрение вершины
-        */
-
         double angle = 3.141592653589793 / (double)arms;
         GeneralPath path = new GeneralPath();
         int i = 0;
--- TitlesPanel.java
+++ TitlesPanel.java
@@ -24,11 +24,28 @@ implements ActionListener {
     private boolean is_done = true;
     private int start_angle = 0;
     private int shape;
+    private String TypeOfChoiseFigure;
+    private int BorderThickness;
+    private boolean Gradient;
+    private boolean ColorRed;
+    private boolean SmalVariantOfTypeParametrs;
 
-    public TitlesPanel(int _shape) {
-/** Инициализация, получаем параметр для выполнения варианта*/
-
-        this.shape = _shape;
+    public TitlesPanel(String _TypeOfChoiseFigure, int _BorderThickness)
+    {        
+    	this.SmalVariantOfTypeParametrs = true;
+        this.TypeOfChoiseFigure = _TypeOfChoiseFigure;
+        this.BorderThickness = _BorderThickness;
+        this.animation = new Timer(50, this);
+        this.animation.setInitialDelay(50);
+        this.animation.start();
+    }
+    public TitlesPanel(String _TypeOfChoiseFigure, int _BorderThickness, boolean _Gradient, boolean _ColorRed)
+    {        
+    	this.SmalVariantOfTypeParametrs = false;
+    	this.ColorRed = _ColorRed;
+    	this.Gradient = _Gradient;
+        this.TypeOfChoiseFigure = _TypeOfChoiseFigure;
+        this.BorderThickness = _BorderThickness;
         this.animation = new Timer(50, this);
         this.animation.setInitialDelay(50);
         this.animation.start();
@@ -42,7 +59,6 @@ implements ActionListener {
     }
 
     private void doDrawing(Graphics g) {
-/** Метод для рисования фигур*/
         this.is_done = false;
         this.g2d = (Graphics2D)g;
         this.g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
@@ -50,9 +66,13 @@ implements ActionListener {
         Insets insets = this.getInsets();
         int w = size.width - insets.left - insets.right;
         int h = size.height - insets.top - insets.bottom;
-        ShapeFactory shape = new ShapeFactory(this.shape);
+        if (this.SmalVariantOfTypeParametrs == true)       
+        {        
+        	ShapeFactory shape = new ShapeFactory(this.TypeOfChoiseFigure, this.BorderThickness);        
+        
         this.g2d.setStroke(shape.stroke);
         this.g2d.setPaint(shape.paint);
+              
         double angle = this.start_angle++;
         if (this.start_angle > 360) {
             this.start_angle = 0;
@@ -70,6 +90,32 @@ implements ActionListener {
                 i = (int)((double)i + (double)shape.width * 1.5);
             }
             j = (int)((double)j + (double)shape.height * 1.5);
+        }
+        }
+        else
+        {
+              	ShapeFactory shape = new ShapeFactory(this.TypeOfChoiseFigure, this.BorderThickness, this.Gradient, this.ColorRed); 
+                this.g2d.setStroke(shape.stroke);
+                this.g2d.setPaint(shape.paint);
+                      
+                double angle = this.start_angle++;
+                if (this.start_angle > 360) {
+                    this.start_angle = 0;
+                }
+                double dr = 90.0 / ((double)w / ((double)shape.width * 1.5));
+                int j = shape.height;
+                while (j < h) {
+                    int i = shape.width;
+                    while (i < w) {
+                        angle = angle > 360.0 ? 0.0 : angle + dr;
+                        AffineTransform transform = new AffineTransform();
+                        transform.translate(i, j);
+                        transform.rotate(Math.toRadians(angle));
+                        this.g2d.draw(transform.createTransformedShape(shape.shape));
+                        i = (int)((double)i + (double)shape.width * 1.5);
+                    }
+                    j = (int)((double)j + (double)shape.height * 1.5);
+                }
         }
         this.is_done = true;
     }
